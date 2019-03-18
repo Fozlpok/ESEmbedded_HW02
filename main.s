de1bfc9def02c32375e07265294432064acf83ef
Fozlpok <Fozlpok@gmail.com>
2019-03-19 02:23:52 +0800

    HW02

--- main.s
+++ main.s
@@ -6,21 +6,31 @@
 .global _start
 .type _start, %function
 _start:
-	nop
+	//set
+	mov r0, #001
+	mov r1, #002
+	mov r2, #003
+	
+	push {r0}
+	push {r1}
+	push {r2}
 
-	//
-	//branch w/o link
-	//
-	b	label01
+	pop {r3}
+	pop {r4}
+	pop {r5}
+ 
+	mov r3, #004
+	mov r4, #005 
+	mov r5, #006 
+	
+	push {r0,r1,r2}	
+	pop {r3,r4,r5}
 
-label01:
-	nop
+	mov r3, #004 
+	mov r4, #005 
+	mov r5, #006 
+	
+	push {r2,r1,r0}
+	pop {r5,r4,r3}
 
-	//
-	//branch w/ link
-	//
-	bl	sleep
-
-sleep:
-	nop
 	b	.
